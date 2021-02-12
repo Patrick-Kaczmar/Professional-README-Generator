91f6c5f77d52d6ee61c07bfe089892da6d52e39a
Zhylow <pk1blue@yahoo.com>
2021-02-12 17:49:18 -0500

    removed spaces from name objects

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer")
-const sf = require("sf")
+// const sf = require("sf")
 const generateMarkdown = require("./generateMarkdown")
 // TODO: Create an array of questions for user input
 inquirer.prompt([
@@ -17,28 +17,28 @@ inquirer.prompt([
     {
         type: "input",
         message: "please add installation instructions:",
-        name: "installation"
+        name: "installation",
     },
     {
         type: "input",
         message: "please add any usage information:",
-        name: "usage information",
+        name: "usageInformation",
     },
     {
         type: "input",
         message: "please add any contribution guidelines:",
-        name: "contribution guidelines"
+        name: "contributionGuidelines",
     },
     {
         type: "input",
         message: "please add the test instructions:",
-        name: "test instructions",
+        name: "testInstructions",
     },
     {
         type: "list",
         message: "please pick a license for your application:",
         choices: ["None", "Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause Simplified License", "BSD 3-Clause New or Revised License", "Boost Software License 1.0", "Creative Commons Zero v1.0 universal", "Eclipse Public License 2.0", "GNU Affero general Public License v3.0", "GNU General Publice License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The unlicense"],
-        name: "License"
+        name: "License",
     },
     {
         type: "input",
@@ -50,7 +50,9 @@ inquirer.prompt([
         message: "what is your email address?",
         name: "email",
     }
-]).then()
+]).then((response) => {
+    console.log(response)
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
