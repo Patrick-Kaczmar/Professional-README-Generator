409ef1d17314c7a3df5f2a27eb2afac8cad93a08
Zhylow <pk1blue@yahoo.com>
2021-02-17 17:33:56 -0500

    fixed license returning empty string when none is selected

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -32,15 +32,11 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license, data) {
-  if (license === license === "None") {
-    return "There is no license"
+  if (license === "None") {
+    return ""
   }else if (license === "Apache License 2.0") {
     return `Copyright ${new Date().getFullYear()} ${data.username}
 
